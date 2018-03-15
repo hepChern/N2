cdc62fb98233801627f4e4a8dc7b8bf84e8eb929
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2018-03-15 21:54:49 +0800

    Impress: 1a592fa308814e759d1a38f187938625

--- Generation/generation/.chern/config.py
+++ Generation/generation/.chern/config.py
@@ -1,5 +1,5 @@
+impression=u'1a592fa308814e759d1a38f187938625'
 object_type=u'task'
-pred_impression=['1f73933aaa1a456684491a050779cbcc']
+pred_impression=['af781557b32d4059bf67221babcec2cf']
 predecessors=['Generation/algorithm']
 successors=['Selection/generation']
-impression=u'5b3af5dd02d0425b9c66eab2b39dc167'
