101758323455bc94b8e6e2751aeee3be97575514
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2018-03-15 21:37:24 +0800

    Impress: 5b3af5dd02d0425b9c66eab2b39dc167

--- Generation/generation/.chern/config.py
+++ Generation/generation/.chern/config.py
@@ -1,1 +1,5 @@
-object_type = "task"
+object_type=u'task'
+pred_impression=['1f73933aaa1a456684491a050779cbcc']
+predecessors=['Generation/algorithm']
+successors=['Selection/generation']
+impression=u'5b3af5dd02d0425b9c66eab2b39dc167'
