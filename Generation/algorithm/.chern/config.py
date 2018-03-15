9eab136ace777290383fe060095d9a78d8c77f57
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2018-03-15 21:37:24 +0800

    Impress: 1f73933aaa1a456684491a050779cbcc

--- Generation/algorithm/.chern/config.py
+++ Generation/algorithm/.chern/config.py
@@ -1,2 +1,4 @@
-object_type = "algorithm"
-main_file = "main.py"
+main_file=u'main.py'
+object_type=u'algorithm'
+successors=['Generation/generation']
+impression=u'1f73933aaa1a456684491a050779cbcc'
